91d2a9d667f041ac576e3409991007acaa98f3f0
Aman Kumar Gupta <amanjobs1234@gmail.com>
2025-09-30 15:23:59 +0000

    name is too long can we give name only Resume Buidler by Aman

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'OpenAman Resume builder',
+  title: 'Resume Builder by Aman',
   description: 'Free Full-Stack Resume Maker',
 };
 
