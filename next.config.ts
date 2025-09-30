40294ebe78e987cecd247746e22a49a1f5ccebe7
Aman Kumar Gupta <amanjobs1234@gmail.com>
2025-09-30 15:30:30 +0000

    Runtime Error
    Error: Invalid src prop (https://jobbinge.in/wp-content/u

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'jobbinge.in',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
